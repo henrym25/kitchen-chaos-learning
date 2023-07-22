c5d3353c43df72b4c9d326751b0727e0f8acab7b
henrym25 <henry3399@hotmail.co.nz>
2023-07-22 20:16:19 +1200

    Added player texture and walking animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour {
 
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
 
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
@@ -30,7 +31,15 @@ public class PlayerController : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
-        Debug.Log(Time.deltaTime);
+            
+
+        isWalking = moveDir != Vector3.zero;
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
